775fc61afb44f7d0b121b0b60db8f2f536f33ea7
MartL7 <latiapelitos123@gmail.com>
2023-12-14 15:57:09 -0600

    Update: Responsive

--- Js/app.js
+++ Js/app.js
@@ -2,32 +2,24 @@
 function Agregar() {
     Swal.fire({
         title: "Advertencia",
-        text: "Pronto Estará Disponible",
+        html: '<span class="text-secondary"> Pronto Estará Disponible </span>',
         icon: "warning",
         confirmButtonText: "Volver",
         footer:
-        '<span class="Texto-Rojo"> Estoy trabajando en ello :( </span>',
-        background: "#000",
+        '<span class="text-secondary"> Estoy trabajando en ello :( </span>',
         allowOutsideClick: false,
         buttonsStyling: false,
         customClass: {
-            popup: "Contenedor-Alerta",
+            popup: "bg-dark",
             confirmButton: "Boton"
         },
     })
 }
 
-function TemaOscuro() {
-    document.querySelector("html").setAttribute("data-bs-theme", "dark");
-    document.querySelector("#icon").setAttribute("class", "bi bi-sun-fill");
-}
-
-function TemaClaro() {
-    document.querySelector("html").setAttribute("data-bs-theme", "light");
-    document.querySelector("#icon").setAttribute("class", "bi bi-moon-fill");
-}
-
 function CambiarTema() {
-    document.querySelector("html").getAttribute("data-bs-theme") === "light" ?
-        TemaOscuro() : TemaClaro();
+    const Pagina = document.querySelector("html");
+    const Icono = document.querySelector("#icon");
+    
+    Pagina.setAttribute("data-bs-theme", Pagina.getAttribute("data-bs-theme") === "light" ? "dark" : "light");
+    Icono.setAttribute("class", Pagina.getAttribute("data-bs-theme") === "light" ? "bi bi-moon-stars-fill" : "bi bi-sun-fill");
 }
